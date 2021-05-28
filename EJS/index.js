e44f0924748a3aba79be5822ba73b9bcc1b7887a
williamg1750 <william.guan12@gmail.com>
2021-05-28 10:51:12 -0400

    dummy data

--- EJS/index.js
+++ EJS/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+//sets the views to EJS
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
   res.render('home.ejs');
 });
 
+app.get('/random', (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1;
+  res.render('random.ejs', { random: num });
+});
+
+app.get('/cats', (req, res) => {
+  const cats = ['blue', 'red', 'green', 'grey', 'orange'];
+  res.render('cats.ejs', { cats });
+});
+
+app.get('/r/:subreddit', (req, res) => {
+  const { subreddit } = req.params;
+  res.render('subreddit.ejs', { subreddit });
+});
 // app.get('*', (req, res) => {});
 
 app.listen(8080, () => {
