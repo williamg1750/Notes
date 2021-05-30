eefc8a1f06c6c2135fce053b2e4fe65089ad4180
williamg1750 <william.guan12@gmail.com>
2021-05-30 16:34:53 -0400

    uuid

--- PostRoute/index.js
+++ PostRoute/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+//an npm packaget hat give unique ids
+//copied from the npm docs as is
+//const { v4: uuidv4 } = require('uuid');
+//renamed  uuidv4 to just uuid easer to use
+const { v4: uuid } = require('uuid');
+
 //this basically is a middelware taht parses thru reqbody as urlencoded data other wise it would be undefined
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,18 +17,22 @@ app.set('view engine', 'ejs');
 
 const comments = [
   {
+    id: 1,
     username: 'william',
     comment: 'bonkgang',
   },
   {
+    id: 2,
     username: 'fandy',
     comment: 'my heart hurts',
   },
   {
+    id: 3,
     username: 'andy',
     comment: 'i love drama',
   },
   {
+    id: 4,
     username: 'kenny',
     comment: 'what should i eat',
   },
@@ -40,6 +50,12 @@ app.post('/comments', (req, res) => {
   const { username, comment } = req.body;
   comments.push({ username, comment });
   res.redirect('/comments');
+});
+
+app.get('/comments/:id', (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((current) => current.id === parseInt(id));
+  res.render('comments/show', { comment });
 });
 
 app.get('/tacos', (req, res) => {
