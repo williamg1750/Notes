2187752867198ec1caac81bd41ac2dc78dec9e9b
williamg1750 <william.guan12@gmail.com>
2021-05-31 10:34:23 -0400

    req patch

--- PostRoute/index.js
+++ PostRoute/index.js
@@ -5,34 +5,37 @@ const path = require('path');
 //an npm packaget hat give unique ids
 //copied from the npm docs as is
 //const { v4: uuidv4 } = require('uuid');
+//uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 //renamed  uuidv4 to just uuid easer to use
+//uuid(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 const { v4: uuid } = require('uuid');
 
 //this basically is a middelware taht parses thru reqbody as urlencoded data other wise it would be undefined
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//need to use inorder to use EJS
+//this basically sets the absoulte path
 app.set('views', path.join(__dirname, 'views'));
+//need to use inorder to use EJS
 app.set('view engine', 'ejs');
 
 const comments = [
   {
-    id: 1,
+    id: uuid(),
     username: 'william',
     comment: 'bonkgang',
   },
   {
-    id: 2,
+    id: uuid(),
     username: 'fandy',
     comment: 'my heart hurts',
   },
   {
-    id: 3,
+    id: uuid(),
     username: 'andy',
     comment: 'i love drama',
   },
   {
-    id: 4,
+    id: uuid(),
     username: 'kenny',
     comment: 'what should i eat',
   },
@@ -48,14 +51,21 @@ app.get('/comments/new', (req, res) => {
 
 app.post('/comments', (req, res) => {
   const { username, comment } = req.body;
-  comments.push({ username, comment });
+  comments.push({ username, comment, id: uuid() });
   res.redirect('/comments');
 });
 
 app.get('/comments/:id', (req, res) => {
   const { id } = req.params;
-  const comment = comments.find((current) => current.id === parseInt(id));
+  const comment = comments.find((current) => current.id === id);
   res.render('comments/show', { comment });
+});
+
+app.patch('/comments/:id', (req, res) => {
+  // const { id } = req.params;
+  // const comment = comments.find((current) => current.id === id);
+  // res.render('comments/show', { comment });
+  res.send('IM THE PATCH');
 });
 
 app.get('/tacos', (req, res) => {
